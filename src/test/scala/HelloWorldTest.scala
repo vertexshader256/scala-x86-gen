2fffed47f0bac30706ca2a7b5ab29546b463b01e
bdwashbu <bdwashbu@gmail.com>
2015-02-05 08:05:57 -0600

    preliminary version of schema complete!

--- src/test/scala/HelloWorldTest.scala
+++ src/test/scala/HelloWorldTest.scala
@@ -21,14 +21,12 @@ import javax.xml.validation.Validator;
     var exceptions = ListBuffer[SAXParseException]()
     var saxParseException: SAXParseException = null
     override def error(exception: SAXParseException) {
-      //println("XML SCHEMA ERROR: " + exception);
       numErrors += 1
      validationError = true;
      saxParseException = exception;
      exceptions += exception
     }     
     override def fatalError(exception: SAXParseException) {
-     // println("XML SCHEMA ERROR: " + exception);
      validationError = true;      
      saxParseException = exception; 
      exceptions += exception
@@ -41,16 +39,8 @@ class HelloWorldTest extends FunSuite {
 
   val executableName = "test_HelloWorldTest.exe"
   
-  //"The x86 reference XML file" should "validate with the schema" in {
-//   val parser = new SAXParser()
-//    parser.setFeature("http://xml.org/sax/features/validation", true); 
-//    parser.setFeature("http://apache.org/xml/features/validation/schema", true);
-//    parser.setFeature("http://apache.org/xml/features/validation/schema-full-checking", true);
     val xsd = new File("x86.xsd")
     val x86ref = new File("x86reference.xml")
-//    parser.setProperty(
-//     "http://apache.org/xml/properties/schema/external-noNamespaceSchemaLocation",
-//     xsd.toURI.toString);
     
     val sf = SchemaFactory.newInstance(
       "http://www.w3.org/XML/XMLSchema/v1.1");
@@ -58,20 +48,22 @@ class HelloWorldTest extends FunSuite {
     val v = s.newValidator();
     val src = new StreamSource(x86ref);
     val handler = new Validator();
-    v.setErrorHandler(handler)
-    
-    //val handler = new Validator();
-    //parser.setErrorHandler(handler);
-    
+    v.setErrorHandler(handler)  
 
     val result: Try[List[SAXParseException]] = Try{v.validate(src); handler.exceptions.toList}
     val failures = result getOrElse handler.exceptions.toList
-    var i = 0
-    failures.take(10) foreach { x =>
-      test("XML Schema validation failure " + i) {
-        assert(false, "- Validation error on line " + x.getLineNumber + ": " + x.getMessage)
+    if (!failures.isEmpty) {
+      var i = 0
+      failures.take(10) foreach { x =>
+        test("XML Schema validation failure " + i) {
+          assert(false, "- Validation error on line " + x.getLineNumber + ": " + x.getMessage)
+        }
+        i+=1
       }
-      i+=1
+    } else {
+      test("x86 reference XML validation") {
+        assert(true, "The x86 reference XML validated against the schema!")
+      }
     }
   
 }
